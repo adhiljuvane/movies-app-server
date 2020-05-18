19f2f82f3b40a3040eb7965258a49cf011e85bd0
AJ <adhilmp3@gmail.com>
2020-05-18 17:57:16 +0530

    final16

--- index.js
+++ index.js
@@ -46,10 +46,10 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use("/", express.static(path.join(__dirname, "build")));
+app.use("/", express.static(path.join(__dirname, "build", "carpe-diem")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "carpe-diem", "index.html"));
 });
 
 app.use("/api/users", require("./routes/users"));
