315b2a95abd64a5c90ee5eeb23b850732de1d13b
AJ <adhilmp3@gmail.com>
2020-05-18 17:19:24 +0530

    finalTen

--- index.js
+++ index.js
@@ -45,10 +45,10 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use("/", express.static(path.join(__dirname, "/client/build")));
+app.use("/", express.static(path.join(__dirname, "client/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
 app.use("/api/users", require("./routes/users"));
